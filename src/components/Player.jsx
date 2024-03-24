7eaeef4aa4cbe50287788c21da59a6196d742089
Anano Topuria <anano.topuriaa@gmail.com>
2024-03-24 18:32:02 +0400

    feat: add functionality to switch active player in game

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,22 +1,24 @@
 import { useState } from "react";
 
-export default function Pleyer({ initialName, symbol }) {
-   const [playerName, setPlayerName] = useState(initialName);
+export default function Pleyer({ initialName, symbol, isActive }) {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
   function handleEditClick() {
     setIsEditing((editing) => !editing);
   }
 
-  function handleChange(event){
-    setPlayerName(event.target.value); 
+  function handleChange(event) {
+    setPlayerName(event.target.value);
   }
 
   let editablePlayerName = <span className="player">{playerName}</span>;
   if (isEditing) {
-    editablePlayerName = <input type="text" required value={playerName} onChange={handleChange}/>;
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
   }
   return (
-    <li>
+    <li className={isActive ? "active" : undefined}>
       <span className="player">
         {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
